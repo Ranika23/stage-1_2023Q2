5f3efa07765bac68d494b2da4572746d3e28c05c
Ranika23 <137180140+Ranika23@users.noreply.github.com>
2023-07-30 07:26:32 +0300

    feat: correct index.js

--- library/index.js
+++ library/index.js
@@ -1,11 +1,11 @@
 console.log(`
-    1. Task: (https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part1.md)
+    1. Task: (https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part1.md);
     \n
-    2. Deploy: (https://rolling-scopes-school.github.io/Ranika23-JSFEPRESCHOOL2023Q2/library/)
+    2. Deploy: (https://rolling-scopes-school.github.io/ranika23-JSFEPRESCHOOL2023Q2/library/);
     \n
-    3. Done 29.07.2023 / deadline 30.07.2023
+    3. Done 29.07.2023 / deadline 30.07.2023;
     \n
-    4. Score : 100 / 100
+    4. Score : 100 / 100;
     \n
     - (10/10) Вёрстка валидная; 
     \n
